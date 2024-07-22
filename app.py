a3d175d10839d5847d75b5b1e2f9b5a8b7109f02
kerariadne <149896206+kerariadne@users.noreply.github.com>
2024-07-22 21:30:44 +0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import time
+import socket
+from flask import Flask
+
+app = Flask(__name__)
+
+START = time.time()
+
+def elapsed():
+    running = time.time() - START
+    minutes, seconds = divmod(running, 60)
+    hours, minutes = divmod(minutes, 60)
+    return "%d:%02d:%02d" % (hours, minutes, seconds)
+
+@app.route('/')
+def root():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return f"Hello World (Python)! (up {elapsed()})\nInstance IP: {ip_address}\n"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8080)
